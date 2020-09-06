8d0064517fb4d53b6a5b897596ed141a0a9711cd
Siju Samson <sijusamson@gmail.com>
2020-09-06 21:05:03 +0530

    Changes for update order for Store

--- routes/apis/store/order.js
+++ routes/apis/store/order.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const path = require('path').resolve;
 const OrderController = require('../../../controllers/admin/order');
-const OrderControllerStore = require('../../../controllers/store/order');
+const StoreOrderController = require('../../../controllers/store/order');
 
 router.get('/', OrderController.getOrders);
 router.put('/:id', OrderController.updateOrder);
-router.put('/status/:id', OrderControllerStore.updateOrder);  
+router.put('/status/:id', StoreOrderController.updateOrder);  
 router.get('/today-count', OrderController.getTodayOrdersCount);
 
 module.exports = router;

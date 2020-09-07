165276976eab7e26f1a755a8a48928c8b6ad18c5
shivam <shivam.soni@enukesoftware.com>
2020-09-07 10:49:10 +0530

    store order status changes

--- controllers/store/order.js
+++ controllers/store/order.js
@@ -22,31 +22,17 @@ module.exports = {
       if (!request.status) {
         throw new apiError.ValidationError('status', messages.STATUS_REQUIRED);
       }
-      if (!request.store_id) {
-        throw new apiError.ValidationError('store_id', messages.STORE_ID_REQUIRED);
-      }
-
+     
       const order = await OrderService.getOrder({ _id: orderId });
       if (!order) {
         throw new apiError.NotFoundError('order_id', messages.ORDER_ID_INVALID);
       }
       
-      const store = await StoreService.getStore({ _id: request.store_id });
-      if (!store) {
-        throw new apiError.ValidationError('store_id', messages.STORE_ID_INVALID);
-      }
-
-      const type = req._userInfo._user_type;
-
-      if (type === 2) {
-        if (request.store_id !== req._userInfo._user_id) throw new apiError.ValidationError('store_id', messages.ID_INVALID);
-      }
-   
       if (order.status===request.status) {
         throw new apiError.ResourceAlreadyExistError('status', messages.STATUS_ALREADY_EXIST);
       }
       
-      if(request.status === 7){
+      if(request.status === 1 || request.status === 7){
         const updateOrder = await OrderService.updateOrder(request, { _id: orderId });
         return res.status(200).send(ResponseService.success({ updateOrder }));
       }else{

8c7583d606ca74976db62c4d5bc0c33e6008d88a
Siju Samson <sijusamson@gmail.com>
2020-09-10 11:34:32 +0530

    Today Orders

--- routes/apis/store/order.js
+++ routes/apis/store/order.js
@@ -5,6 +5,7 @@ const OrderController = require('../../../controllers/admin/order');
 const StoreOrderController = require('../../../controllers/store/order');
 
 router.get('/', OrderController.getOrders);
+router.get('/today-orders', OrderController.getTodayOrders);
 router.put('/:id', OrderController.updateOrder);
 router.put('/status/:id', StoreOrderController.updateOrder);  
 router.get('/today-count', OrderController.getTodayOrdersCount);

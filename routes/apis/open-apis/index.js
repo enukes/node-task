e6e0fdbe3dd78d5f804aa59842b16db552a41310
Siju Samson <sijusamson@gmail.com>
2020-09-10 13:42:41 +0530

    Changes for get cities and areas completed

--- routes/apis/open-apis/index.js
+++ routes/apis/open-apis/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const OpenApiController = require('../../../controllers/common/open-apis');
 const Upload = require('../../../common/multer');
+const AreaController = require('../../../controllers/customer/area');
 
 // router.post('/', dsd)
 router.post('/driver-register', OpenApiController.addDriver);
@@ -10,6 +11,8 @@ router.post('/verify-order', OpenApiController.verifyOrder);
 router.post('/store-register', Upload.any(), OpenApiController.createStore.bind(OpenApiController));
 router.get('/stores-by-subcategory', OpenApiController.getAllStoreBySubCategory);
 router.get('/categories', OpenApiController.getAllCategoriesForStoreRegister);
+router.get('/cities', AreaController.getCitiesList);
+router.get('/areas', AreaController.getAreasList);
 
 
 
